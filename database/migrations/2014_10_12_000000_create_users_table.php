56c6a21a1368f485ba4be481909de4c3a1908b51
Faisal Ramadhan <faisalramadhan041201@gmail.com>
2022-12-10 06:48:43 +0700

    first build

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email');
+            $table->char('mobile', 13)->unique();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserAPIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/* User Route */
+Route::get('user', [UserAPIController::class, 'index'])->name('user.index');
+
+
