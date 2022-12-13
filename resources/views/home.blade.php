0c8897a9d6f146e34bb933c1e2b6a782130fd7f5
Faisal Ramadhan <faisalramadhan041201@gmail.com>
2022-12-13 07:29:49 +0700

    updates on main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,5 @@
 <body>
     @include('includes.home-content')
     <script src="{{ url('web.js')}}"></script>
-    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
 </body>
 </html>
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,14 +1,10 @@
 <!-- head template -->
 <meta charset="utf-8">
-
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
 rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" 
 crossorigin="anonymous" />
-
-<link rel="stylesheet" href="{{ url('web.css')}}" />
-
+<link rel="stylesheet" href="{{ url('web.css') }}" media="screen and (min-width: 600px)"/>
+<link rel="stylesheet" href="{{ url('mobile.css') }}" media="screen and (max-width: 600px)"/>
 <link rel="shortcut icon" href="{{ url('image/TERANG.png')}}" type="image/x-icon">
-
 <title>{{ config('app.name') }}</title>
